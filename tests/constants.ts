23a43703b6d8c83e31b291f6e687ab466bda8461
Róbert Darida <rdarida@users.noreply.github.com>
2025-01-27 19:06:52 +0100

    test: create data folder in .temp

--- tests/constants.ts
+++ tests/constants.ts
@@ -2,12 +2,14 @@ import { join } from 'path';
 
 export const TEMP_DIR = join(__dirname, '.temp');
 
+export const DATA_DIR = join(TEMP_DIR, 'data');
+
 export const FILE_PATHS = [
   'test file 1.txt',
   'test file 2.md',
   join('inner dir', 'test file 3.txt'),
   join('inner dir', 'test file 4.md')
-].map(fileName => join(TEMP_DIR, fileName));
+].map(fileName => join(DATA_DIR, fileName));
 
 export const ZIP_PATH = join(TEMP_DIR, 'test zip.7z');
 
--- tests/setup.ts
+++ tests/setup.ts
@@ -4,17 +4,17 @@ import { execSync } from 'child_process';
 import { EOL } from 'os';
 
 import { getSevenZipPath } from '../src/utils';
-import { FILE_PATHS, TEMP_DIR } from './constants';
+import { DATA_DIR, FILE_PATHS } from './constants';
 
 export default function globalSetup(): void {
-  const innerDir = join(TEMP_DIR, 'inner dir');
+  const innerDir = join(DATA_DIR, 'inner dir');
   mkdirSync(innerDir, { recursive: true });
 
   FILE_PATHS.forEach(path => {
-    const content = `Hello, ${path.replace(TEMP_DIR, '')}` + EOL;
+    const content = `Hello, ${path.replace(DATA_DIR, '')}` + EOL;
     writeFileSync(path, content);
   });
 
   const seven = getSevenZipPath();
-  execSync(`${seven} a -mx1 "test zip.7z"`, { cwd: TEMP_DIR });
+  execSync(`${seven} a -mx1 "../test zip.7z"`, { cwd: DATA_DIR });
 }
